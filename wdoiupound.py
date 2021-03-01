fd438e7856d19f811453c09dd2d243b6568b6399
Salvador IBM <salvaib@gmail.com>
2021-03-01 09:32:18 +0000

    Created Database models with Flask-SQLAlchemy

--- wdoiupound.py
+++ wdoiupound.py
@@ -1,15 +1,43 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Credential(db.Model):
+    __tablename__ = 'credentials'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='credential')
+
+    def __repr__(self):
+        return '<Credential %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Comumn(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    credential_id = db.Column(db.Integer, id.ForeignKey('credentials.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
